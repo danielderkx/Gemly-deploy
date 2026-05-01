0e083074fb1190558a331d5247dcdea51f63d142
danielderkx <danielderkx@gmail.com>
2026-05-01 14:49:15 +0200

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+'use client'
+import Scanner from './Scanner'
+
+export default function Home() {
+  return (
+    <div style={{ width: '100%', maxWidth: 540 }}>
+      <Scanner />
+    </div>
+  )
+}
